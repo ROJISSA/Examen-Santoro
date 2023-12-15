5b1357a63bf91c0a6114da49b32e42a21682c37c
ROJISSA <141443385+ROJISSA@users.noreply.github.com>
2023-12-15 16:41:52 -0300

    evalmain.c

---
+++ src/evalmain.c
@@ -0,0 +1,8 @@
+#include "MKL254.h"
+#include "eval.h"
+
+int main{
+    init_eval(3, A);
+    bcd(calculos(leer_pines1(), +, leer_pines2()));
+    bcd(calculos(leer_pines1(), -, leer_pines2()));
+}
